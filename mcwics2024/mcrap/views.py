a2d0910e03f29b5cc1b4f6d93a42b49ca72a818c
Dashiell Rich <dashiellr@Dashiells-MacBook-Pro.local>
2024-02-10 17:08:39 -0500

    ADD: createRap func

--- mcwics2024/mcrap/views.py
+++ mcwics2024/mcrap/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .forms import TextForm
 import os
-import openai
-openai_key = os.environ.get('OPENAI_KEY')
+import vertexai as vai
+from vertexai.language_models import TextGenerationModel
+openai_key = os.getenv('GEMINI_KEY')
 
 def mcrap(request):
     return render(request, 'mcrap/main.html')
@@ -12,14 +13,23 @@ def rapTransform(request):
         form = TextForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['text']
-            openai.api_key=openai_key
-            response = openai.ChatCompletion.create(
-                model="gpt-4", 
-                messages=[{"role": "system", "content": "You are the most talented rap lyricist in the world."},
-                          {"role": "user", "content": f"Convert the following text into a rap song: {text}"}]
-            )
-            rap_song = response.choices[0].message['content']
+            rap_song = createRap(256, 'mcwics2024', 'us-central1', text)
             return render(request, 'mcrap/output', {'rap_song': rap_song})
     else: 
         form = TextForm()
     return request(request, 'mcrap/main.html', {'form': form})
+
+def createRap(temperature, project_id, location, input):
+    vai.init(project_id, location)
+    params = {
+        "temperature": temperature,
+        "max_output_tokens": 1024,
+        "top_p": 0.8,
+        "top_k": 40,
+    }
+    model = TextGenerationModel.from_pretrained("text-bison@001")
+    response = model.predict(f"Imagine you're the greatest rap lyricist in the world, 
+                             create a rap song based on this text: {input}.",
+                               **params)   
+    print(f"Response from model: {response.text}")
+    return response.text
