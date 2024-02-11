79b86f94640cf811e2a5076a6ba4a233c9c81149
Dashiell Rich <dashiellr@Dashiells-MacBook-Pro.local>
2024-02-11 00:17:02 -0500

    fixed tts

--- mcwics2024/mcrap/views.py
+++ mcwics2024/mcrap/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
 from .forms import TextForm
 import cohere
 from gtts import gTTS
 from pydub import AudioSegment
 from pydub.playback import play
+from django.utils.safestring import mark_safe
+import json
 
 
 client = cohere.Client('xiRQw9XMonQhgxXa3x2WLAfuG7tdQwBAerIEPiUz')
@@ -11,19 +14,22 @@ def mcrap(request):
     return render(request, 'mcrap/main.html')
 
 def rap_transform(request):
+    # Initialize clean_lyrics_json with an empty list or default value
+    clean_lyrics_json = mark_safe(json.dumps([]))
+
     if request.method == "POST":
         rap_song_input = request.POST.get('rap_song')
         if rap_song_input:
-            rap_song = createRap(rap_song_input)  
+            rap_song = createRap(rap_song_input)
             rap_split = rap_song.split('\n')
-            clean_lyrics = []
-            for lyric in rap_split:
-                if "(" not in lyric and "[" not in lyric and ":" not in lyric:
-                    clean_lyrics.append(lyric)
-            clean_lyrics = clean_lyrics[:-1]
-            
-            return render(request, 'mcrap/output.html', {'rap_song': clean_lyrics}) 
-    return render(request, 'mcrap/main.html')
+            clean_lyrics = [lyric for lyric in rap_split if "(" not in lyric and "[" not in lyric and ":" not in lyric]
+            clean_lyrics = clean_lyrics[:-1]  # Assuming you want to remove the last element for some reason
+
+            # Update clean_lyrics_json within the if block
+            clean_lyrics_json = mark_safe(json.dumps(clean_lyrics))
+
+    # Ensure clean_lyrics_json is passed in the context regardless of the POST condition
+    return render(request, 'mcrap/output.html', {'rap_song': clean_lyrics_json})
 
 
 def createRap(text):
